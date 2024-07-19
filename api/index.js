c715997088afa0557eff651186e09210dedcc838
Sarim Bin Asif <sarimbinasif12345@gmail.com>
2024-07-19 22:16:23 +0500

    added cookies on login

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import authRoute from "./routes/auth.js";
 import usersRoute from "./routes/users.js";
 import hotelsRoute from "./routes/hotels.js";
 import roomsRoute from "./routes/rooms.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -21,7 +22,7 @@ const connect = async () => {
   }
 };
 
-
+app.use(cookieParser())
 app.use(express.json())
 
 //middlewares
